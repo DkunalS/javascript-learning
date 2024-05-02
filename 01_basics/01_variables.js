c58af66d72bc0f13970c2b4151a0973bdbd8ca0c
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-05-02 05:51:36 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 1235
+let accountEmail = "kunal@google.com"
+var passwd = "123456" 
+/* 
+var was introduced earlier version of JavaScript but it has the issue of block scope functional scope 
+but in latest version there is use of only let to define the variables
+*/
+accountCity = "Indore" // this type of variable define not prefered to use although we can define in this way also
+let accountState; // in this case variable is undefined because no value assigned to it
+
+
+console.log("Initial Values");
+console.table([accountId, accountEmail, passwd, accountCity, accountState]);
+
+//accountId = 1253 // not Allowed to change because of constant
+accountEmail = "sahu@google.com"
+passwd = "258963"
+accountCity = "Gwaliior"
+
+console.log("Values after Changing");
+
+console.table([accountId, accountEmail, passwd, accountCity, accountState]);

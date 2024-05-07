032137f6039359e66fddd8cf7f9f3994bbed2554
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-05-07 10:05:09 +0000

    commit

---
+++ 01_basics/05_string.js
@@ -0,0 +1,38 @@
+const name = "jassi"
+const stream = "political science"
+
+
+console.log("Hello "+ name + " preparing for " + stream); // this type of comncatination is not prefered 
+
+console.log(`Hello! His name is ${name} and he is preparing for UGC NET in ${stream} stream`); // now this is pefered
+
+// string functions
+
+ const indexAndValue = new String("rang-de-basanti")
+ const stringFunctions = new String("baar baar dekho")
+ const stringFunForTrim = new String("   Andaaz   Apna   Apna   ")
+
+//  console.log(indexAndValue.charAt(5));      // output=> d
+//  console.log(indexAndValue.charAt(16));     // output=> 
+//  console.log(indexAndValue.indexOf('h'));   // output=> -1
+//  console.log(indexAndValue.indexOf('b'));   // output=> 8
+ console.log(indexAndValue.lastIndexOf('a') ); // output=> 11
+
+
+ console.log(stringFunctions.length);          // output=> 15
+ 
+ console.log(indexAndValue.includes('sa'))     // ouput=> true
+
+ console.log(indexAndValue.lastIndexOf('de'))  // output=> 5
+ console.log(stringFunctions.lastIndexOf('ar')); // output=> 7
+
+ console.log(indexAndValue.substring(2,6));      // output => ng-d
+ console.log(indexAndValue.slice(5,10));         // output=> de-ba 
+ // Note- it indicate the character upto but not the indicate last index 10 is not included
+
+ console.log(indexAndValue.trim());           // output=> rang-de-basanti (it works only on whitesapces)
+ 
+ console.log(stringFunForTrim.trim());        // output=> Andaaz   Apna   Apna
+
+ console.log(stringFunctions.split(' '))      // output=> [ 'baar', 'baar', 'dekho' ]
+ console.log(indexAndValue.split('-'));       // output=> [ 'rang', 'de', 'basanti' ]

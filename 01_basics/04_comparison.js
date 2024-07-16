054a2026ecb2f563e50c755d2e62cd7360ad9709
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-16 05:52:59 +0000

    Commit

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,15 +1,18 @@
+/*
 console.log(2 > 1)
 console.log(2 < 1)
 console.log(2 >= 1)
 console.log(2 <= 1)
 console.log(2 == 1)
 console.log(2 != 1)
-
+console.log();
+*/
 
 //these comparisons not prefered
 
 console.log("2" > 1);        // here is type conversion is happening
 console.log("02" > 1);
+console.log();
 
 //The reason is that an equality check == and comparison > < >= <= work differently.
 // Comparisons convert null to a number, treating it as 0 that's why null >= 0 is true and null > 0 is false.
@@ -18,8 +21,8 @@ console.log(null == 0);       // In both the cases output are false
 console.log(null >= 0);       // in case of >= is behave differently, output will be true
 
 
-console.log();
-console.log(undefined == 0);   // in all cases output will be false
+console.log();             // in all cases output will be false
+console.log(undefined == 0);   
 console.log(undefined > 0);
 console.log(undefined >= 0);
 console.log(undefined < 0);

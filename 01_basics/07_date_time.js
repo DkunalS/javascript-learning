4cd5d0d9e1518cb90f74bd8fabc49593727e6dff
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-17 15:13:39 +0000

    Commit

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,28 @@
+
+let curr_date = new Date();
+
+
+console.log(curr_date);                         // output=> 2024-07-17T14:56:39.131Z
+console.log(curr_date.toString());              // output=> Wed Jul 17 2024 14:59:24 GMT+0000 (Coordinated Universal Time)
+console.log(curr_date.toLocaleString());        // output=> 7/17/2024, 3:00:03 PM
+console.log(curr_date.getMinutes());            // output=> 58
+console.log(curr_date.getMinutes());            // output=> 58
+console.log(curr_date.getMinutes());            // output=> 58
+console.log(curr_date.getMinutes());            // output=> 58
+
+
+console.log(curr_date.getDate());               // output=> 17
+console.log(curr_date.getMonth());              // output=> 6
+console.log(curr_date.getDay());                // output=> 3
+
+console.log(curr_date.getFullYear());           // output=> 2024
+console.log(curr_date.getHours());              // output=> 14
+console.log(curr_date.getMinutes());            // output=> 58
+
+
+
+
+
+
+
+

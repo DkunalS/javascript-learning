416b7fa2ce2aeb73f55c6d07f20bcc2336b89dc9
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-18 13:05:25 +0000

    Commit

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -5,11 +5,27 @@
 //object literals - Declaration
 //it is in key value pair
 
+//Declaring the symbol
+const mySymbol = Symbol("key")
+//object declaration
 const jsUser = {
-    name: "kunal",
+    name: "kunal",              // here JS automatically name consider as "name"
+    "full name": "kunal sahu",  // here we are explicitly defining
+    mySymbol: "key2",
+    [mySymbol]: "key1",    // interview Q - symbol used in [] in object 
     age: "27",
     location: "indore",
     email: "indore@gmail.com"
 }
 
 //accessing the object and its values
+console.log(jsUser.age)    // By this method we can access only without " " key's values
+
+console.log(jsUser["full name"]); // By this method we can access any type of object's value
+console.log(jsUser["email"]); // By this method we can access any type of object's value
+
+//interview - access the symbol
+console.log(jsUser["mySymbol"]);
+console.log(typeof jsUser["mySymbol"]);   // typeof this is string, it is not refering to the symbol
+console.log(jsUser[mySymbol]);
+console.log(typeof jsUser[mySymbol]);   // typeof this is symbol, it is not refering to the symbol

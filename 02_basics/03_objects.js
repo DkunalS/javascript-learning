e61719be6d901c34cb6f06deed4cb0dde2a62a4f
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-18 12:22:19 +0000

    Commit

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+// objects can be declared in two ways 
+// 1- Literal  and 
+//2- constructor (singleton) object.create
+
+//object literals - Declaration
+//it is in key value pair
+
+const jsUser = {
+    name: "kunal",
+    age: "27",
+    location: "indore",
+    email: "indore@gmail.com"
+}
+
+//accessing the object and its values

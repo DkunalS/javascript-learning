2096a676d9e05bd5a95dc99b5cf18e06dc586384
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-18 14:46:43 +0000

    Commit

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,10 @@
+/*---------Objects - constructor and singleton--------*/
+
+const userObject = new Object()  //object using Constructor
+
+constructorObject.id = "123abc"
+constructorObject.name = "saime"
+
+console.log(constructorObject);
+
+const 

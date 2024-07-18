b1d8ced2fbe305faa711845d6976219af4f7aaee
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-18 12:13:56 +0000

    Commit

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -29,6 +29,17 @@ const manyInnerArray = [1, 2, 3, 4, [6, 5, 7], 9, [8, 9, [8, 5]]]
 console.log(manyInnerArray.flat(1));  // output=> flat array to 1 inner array
 console.log(manyInnerArray.flat(2));  // output=> flat array to 2 inner array
 
-//----- 
+//-----when data come from other source like web pages so we have to check dat is in array format 
 
+console.log(Array.isArray("kunal"));    // checking is it array or not
+console.log(Array.from("kunal"));       // convert String data into array
 
+//it will not covert into array -here we have to specifywhat we have to convert into array
+console.log(Array.from({name: "hitesh"})); // passing the object- have to specify that convert key/pair to array 
+
+//if we have multiple variable and arrays 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // it will return new array by combining many variables

eff1f7b2e4ee184037a7efad314de8a6d4b50910
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-22 05:01:37 +0000

    Commit

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -25,6 +25,23 @@ if(true){
 //console.log(manish);  // with this throughing error - manish is not defined
 //console.log(jassi);   // same error  - jassi is not defined
 console.log(kunal);     // but with this s output=> 65
-//console.log(kunal1);
 // var and without var creates the scope problem it is accessable from outside of the scope even seperate varible is defined.
 // Issue occurs when many programmer working on the same file and varible declred with the same name
+
+
+console.log("----- Checking local and global scoping with the let and var---");
+
+var dj = 50
+let nagu= 89
+
+if(true){
+    let nagu = 65;
+    var dj = 20;
+    console.log("---within {} local scope--");
+    console.log(nagu);   //output=> 65
+    console.log(dj);     // ouput=> 20
+}
+
+console.log("-----outside of the {}");
+console.log(nagu);  //ouput=> 89
+console.log(dj);    //output=> 20

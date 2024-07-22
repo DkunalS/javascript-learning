23a82078659a097605653578d37c9f1557fa009c
DkunalS <41237524+DkunalS@users.noreply.github.com>
2024-07-22 03:47:13 +0000

    Commit

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -31,3 +31,50 @@ function loginUsrMsg(username = "manish"){  //here providing default value if no
 console.log(loginUsrMsg("kunal")) // output=> kunal just logged in
 console.log(loginUsrMsg(""))    // output=> just logged in(if used username === undefined and incse of !username output=> undefined)
 console.log(loginUsrMsg())    // output=> undefined
+
+
+//-----------When we dont know how many argumnt will be come--------------
+// like shopping cart (no of items can be anything)
+// using the rest opertaor like  ...num1(storing value in the array)
+// 
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 320)); // output=> [ 200, 400, 320 ]
+
+//here first two argment storing in the var1, var2
+function calculateCartPrice1(var1, var2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice1(200, 400, 320, 500)); // output=> [ 320, 500 ]
+
+
+//passing objects in the function
+const user = {
+    username: "kunal",
+    price: 1520
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user) // passing object through variable
+// passing direct object
+handleObject({
+    username: "manish",
+    price: 123
+})
+
+//passing the array in the function
+
+const myArray = [200, 600, 852, 456]
+
+function passingArray(getArray){
+    return getArray[1]
+}
+
+console.log(passingArray(myArray));
